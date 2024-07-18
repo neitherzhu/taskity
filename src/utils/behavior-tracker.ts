18d3b98c2a2088ffbdc712e4060fa2e8de594201
诸炜 <zhuw@shinemo.com>
2024-07-18 17:40:32 +0800

    feat: 优化计时逻辑

--- src/utils/behavior-tracker.ts
+++ src/utils/behavior-tracker.ts
@@ -1,30 +1,47 @@
-import { BrowserWindow, screen } from 'electron'
+import { BrowserWindow, powerMonitor } from 'electron'
 
-let stopMins = 0
 let behaviorTrackerTimer: NodeJS.Timeout
-const MAX_STOP = 60 * 10 // 10分钟无动作则暂停
+const MAX_STOP = 60 // 10分钟无动作则暂停
+let idleTime: number
+
+// export const behaviorTracker = (mainWindow: BrowserWindow) => {
+//   const { x, y } = screen.getCursorScreenPoint()
+//   const lastMousePosition = `${x}-${y}`
+
+//   behaviorTrackerTimer = setTimeout(() => {
+//     const { x, y } = screen.getCursorScreenPoint()
+//     const currentMousePosition = `${x}-${y}`
+//     if (lastMousePosition === currentMousePosition) {
+//       stopMins++
+//       if (stopMins === MAX_STOP) {
+//         mainWindow.webContents.send('behavior-change', true)
+//       }
+//     } else {
+//       stopMins = 0
+//     }
+
+//     behaviorTracker(mainWindow)
+//   }, 1000)
+// }
+
+// export const stopBehaviorTracker = () => {
+//   stopMins = 0
+//   clearTimeout(behaviorTrackerTimer)
+// }
 
 export const behaviorTracker = (mainWindow: BrowserWindow) => {
-  const { x, y } = screen.getCursorScreenPoint()
-  const lastMousePosition = `${x}-${y}`
+  idleTime = powerMonitor.getSystemIdleTime()
 
   behaviorTrackerTimer = setTimeout(() => {
-    const { x, y } = screen.getCursorScreenPoint()
-    const currentMousePosition = `${x}-${y}`
-    if (lastMousePosition === currentMousePosition) {
-      stopMins++
-      if (stopMins === MAX_STOP) {
-        mainWindow.webContents.send('behavior-change', true)
-      }
-    } else {
-      stopMins = 0
-    }
-
     behaviorTracker(mainWindow)
-  }, 1000)
+    if (idleTime > MAX_STOP) {
+      mainWindow.webContents.send('behavior-change', true)
+    } else {
+      behaviorTracker(mainWindow)
+    }
+  }, 10000)
 }
 
 export const stopBehaviorTracker = () => {
-  stopMins = 0
   clearTimeout(behaviorTrackerTimer)
 }
